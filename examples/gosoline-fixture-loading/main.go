22bf8b2ae30a55fbb3fa7e4f8ce6b873f5a91325
bogdanfinn <bogdan.finn@googlemail.com>
2020-03-06 16:26:27 +0100

    fixtures: wrote documentation and example application

---
+++ examples/gosoline-fixture-loading/main.go
@@ -0,0 +1,105 @@
+//+build fixtures
+
+package main
+
+import (
+	"github.com/applike/gosoline/pkg/application"
+	db_repo "github.com/applike/gosoline/pkg/db-repo"
+	"github.com/applike/gosoline/pkg/ddb"
+	"github.com/applike/gosoline/pkg/fixtures"
+	"github.com/applike/gosoline/pkg/mdl"
+)
+
+type DynamoDbExampleModel struct {
+	Name  string `ddb:"key=hash"`
+	Value string `ddb:"global=hash"`
+}
+
+type OrmFixtureExample struct {
+	db_repo.Model
+	Name *string
+}
+
+func main() {
+	app := application.Default(application.WithFixtures(createFixtures()))
+	app.Run()
+}
+
+func createFixtures() []*fixtures.FixtureSet {
+	return []*fixtures.FixtureSet{
+		{
+			Enabled: true,
+			Writer: fixtures.MysqlOrmFixtureWriterFactory(
+				&db_repo.Metadata{
+					ModelId: mdl.ModelId{
+						Name: "orm_fixture_example",
+					},
+				},
+			),
+			Fixtures: []interface{}{
+				&OrmFixtureExample{
+					Model: db_repo.Model{
+						Id: mdl.Uint(1),
+					},
+					Name: mdl.String("example"),
+				},
+			},
+		},
+		{
+			Enabled: true,
+			Writer:  fixtures.PlainMySqlFixtureWriterFactory(),
+			Fixtures: []interface{}{
+				&fixtures.PlainMySqlFixture{
+					TableName: "plain_fixture_example",
+					Columns:   []string{"id", "name"},
+					Values: []fixtures.PlainMySqlFixtureValue{
+						{1, "testName1"},
+						{2, "testName2"},
+					},
+				},
+			},
+		},
+		{
+			Enabled: true,
+			Writer: fixtures.DynamoDbKvStoreFixtureWriterFactory(&mdl.ModelId{
+				Project:     "gosoline",
+				Environment: "dev",
+				Family:      "example",
+				Application: "fixture-loader",
+				Name:        "exampleModel",
+			}),
+			Fixtures: []interface{}{
+				&fixtures.KvStoreFixture{
+					Key:   "SomeKey",
+					Value: &DynamoDbExampleModel{Name: "Some Name", Value: "Some Value"},
+				},
+			},
+		},
+		{
+			Enabled: true,
+			Writer: fixtures.DynamoDbFixtureWriterFactory(&ddb.Settings{
+				ModelId: mdl.ModelId{
+					Project:     "gosoline",
+					Environment: "dev",
+					Family:      "example",
+					Application: "fixture-loader",
+					Name:        "exampleModel",
+				},
+				Main: ddb.MainSettings{
+					Model: DynamoDbExampleModel{},
+				},
+				Global: []ddb.GlobalSettings{
+					{
+						Name:               "IDX_Name",
+						Model:              DynamoDbExampleModel{},
+						ReadCapacityUnits:  1,
+						WriteCapacityUnits: 1,
+					},
+				},
+			}),
+			Fixtures: []interface{}{
+				&DynamoDbExampleModel{Name: "Some Name", Value: "Some Value"},
+			},
+		},
+	}
+}
